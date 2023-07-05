0b6ce4a014cbd4e83d496b51d3297d07749def65
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-04 17:37:36 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
+
 log_file = ARGV[0]
 
-File.readlines(log_file).each do |line|
-  match_data = line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-  
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_data[3]
-    
-    puts "#{sender},#{receiver},#{flags}"
+File.open(log_file, 'r') do |file|
+  file.each_line do |line|
+    if line.include?('SMS')
+      sender = line.match(/\[from:(.*?)\]/)&.captures&.first
+      receiver = line.match(/\[to:(.*?)\]/)&.captures&.first
+      flags = line.match(/\[flags:(.*?)\]/)&.captures&.first
+      
+      puts "#{sender},#{receiver},#{flags}"
+    end
   end
 end
