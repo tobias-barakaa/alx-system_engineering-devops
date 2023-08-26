5b82291df69872e9185f17cc4b03caff48e72087
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-26 12:27:34 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # 2-execute_a_command.pp
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
+  command     => 'pkill -9 -f killmenow',
   path        => ['/bin', '/usr/bin'],
   refreshonly => true,
 }
