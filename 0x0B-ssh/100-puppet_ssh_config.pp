8d7ad9f73981dc5605d89d7bfcfd139f2b8517c0
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-27 12:23:14 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,21 @@
-file { '/etc/ssh/sshd_config':
-  content => "
-# ... other lines ...
-PasswordAuthentication no
-# ... other lines ...
-",
+# Define a class for SSH client configuration
+class ssh_client_config {
+
+  # Ensure the SSH client uses the private key ~/.ssh/school
+  file_line { 'Set SSH IdentityFile':
+    path   => '/etc/ssh/ssh_config', # Path to the SSH client configuration file
+    line   => '    IdentityFile ~/.ssh/school', # Line to add or modify
+    match  => '^ *IdentityFile', # A pattern to match existing lines
+  }
+
+  # Ensure the SSH client refuses to authenticate using a password
+  file_line { 'Disable Password Authentication':
+    path   => '/etc/ssh/ssh_config', # Path to the SSH client configuration file
+    line   => '    PasswordAuthentication no', # Line to add or modify
+    match  => '^ *PasswordAuthentication', # A pattern to match existing lines
+  }
+
 }
 
-file { '/etc/ssh/ssh_config':
-  content => "
-# ... other lines ...
-IdentityFile ~/.ssh/school
-# ... other lines ...
-",
-}
+# Apply the SSH client configuration class
+include ssh_client_config
