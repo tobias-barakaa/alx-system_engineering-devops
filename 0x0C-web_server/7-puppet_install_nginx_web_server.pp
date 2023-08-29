cb8a6f12699e7d558ea8b1de5961e8d78c2a9678
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-30 02:48:39 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,31 +1,47 @@
 # Define the Nginx class
 class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
+    package { 'nginx':
+        ensure => installed,
+    }
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+    service { 'nginx':
+        ensure => running,
+        enable => true,
+        require => Package['nginx'],
+    }
 
-  file { '/etc/nginx/sites-available/default':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => link,
-    target => '/etc/nginx/sites-available/default',
-  }
-
-  ->
-
-  # Redirect all requests to /redirect_me to /
-  # with a 301 redirect.
-  httpd::location { '/redirect_me':
-    ensure => present,
-    redirect => '301 https://example.com/',
-  }
+    file { '/var/www/html/index.html':
+        ensure => file,
+        content => 'Hello World!',
+        require => Package['nginx'],
+    }
 }
 
+# Apply the Nginx class
+include nginx
+
+# Define the Redirect class
+class redirect {
+    file { '/etc/nginx/sites-available/redirect_me':
+        ensure => file,
+        content => "
+        server {
+            listen 80;
+            server_name _;
+            location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+            }
+        }
+        ",
+        require => Class['nginx'],
+    }
+
+    file { '/etc/nginx/sites-enabled/redirect_me':
+        ensure => link,
+        target => '/etc/nginx/sites-available/redirect_me',
+        require => File['/etc/nginx/sites-available/redirect_me'],
+    }
+}
+
+# Apply the Redirect class
+include redirect
