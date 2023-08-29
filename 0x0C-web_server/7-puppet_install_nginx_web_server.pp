bf6bc6a5a37d9493d2ed4cec14e40fc66016ef67
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-30 02:54:41 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,20 @@
 # Define the Nginx class
-class nginx {
-    package { 'nginx':
-        ensure => installed,
-    }
-
-    service { 'nginx':
-        ensure => running,
-        enable => true,
-        require => Package['nginx'],
-    }
-
-    file { '/var/www/html/index.html':
-        ensure => file,
-        content => 'Hello World!',
-        require => Package['nginx'],
-    }
+package { 'nginx':
+  ensure => installed,
 }
 
-# Apply the Nginx class
-include nginx
-
-# Define the Redirect class
-class redirect {
-    file { '/etc/nginx/sites-available/redirect_me':
-        ensure => file,
-        content => "
-        server {
-            listen 80;
-            server_name _;
-            location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            }
-        }
-        ",
-        require => Class['nginx'],
-    }
-
-    file { '/etc/nginx/sites-enabled/redirect_me':
-        ensure => link,
-        target => '/etc/nginx/sites-available/redirect_me',
-        require => File['/etc/nginx/sites-available/redirect_me'],
-    }
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
 }
 
-# Apply the Redirect class
-include redirect
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
