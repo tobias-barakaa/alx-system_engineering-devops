4c0278a1d114fc2c96e8e0a1e30a33f5f8807906
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 02:54:52 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -24,7 +24,6 @@ def export_json():
 
     # Create a dictionary to store the TODO items for all users.
     user_todos = {}
- 
 for user in users:
     user_todos[user['id']] = []
 
@@ -39,9 +38,9 @@ for todo in todos:
     return user_todos
 
 if __name__ == '__main__':
-  # Export the data in JSON format.
+    # Export the data in JSON format.
     user_todos = export_json()
 
-  # Write the JSON data to a file
-  with open('todo_all_employees.json', 'w') as f:
+    # Write the JSON data to a file
+with open('todo_all_employees.json', 'w') as f:
       json.dump(user_todos, f)
