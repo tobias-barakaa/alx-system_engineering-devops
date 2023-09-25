1ed15e58cfe887495db49fb8b2b3271acce88060
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 02:56:56 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 """
-Python script that exports data in the JSON format.
+Python script to export data in the JSON format.
 """
+
 import json
 import requests
 
 
 def export_json():
-    """Exports data in JSON format for all users.
+    """
+    Export data in JSON format for all users.
 
     Returns:
     A dictionary containing the TODO items for all users.
@@ -24,16 +26,17 @@ def export_json():
 
     # Create a dictionary to store the TODO items for all users.
     user_todos = {}
-for user in users:
-    user_todos[user['id']] = []
+    
+    for user in users:
+        user_todos[user['id']] = []
 
     # Add the TODO items to the dictionary for each user.
-for todo in todos:
-    user_todos[todo['userId']].append({
-      'username': users[todo['userId'] - 1]['username'],
-      'task': todo['title'],
-      'completed': todo['completed']
-    })
+    for todo in todos:
+        user_todos[todo['userId']].append({
+            'username': users[todo['userId'] - 1]['username'],
+            'task': todo['title'],
+            'completed': todo['completed']
+        })
 
     return user_todos
 
@@ -41,6 +44,6 @@ if __name__ == '__main__':
     # Export the data in JSON format.
     user_todos = export_json()
 
-    # Write the JSON data to a file
-with open('todo_all_employees.json', 'w') as f:
-      json.dump(user_todos, f)
+    # Write the JSON data to a file.
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(user_todos, f, indent=4)
