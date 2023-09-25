f13d2ad3029bec8fd80102b3002443a6d10a7b52
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 02:29:03 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,22 +7,22 @@ import json
 import requests
 
 def export_json():
-  """Exports data in JSON format for all users.
+    """Exports data in JSON format for all users.
 
-  Returns:
+    Returns:
     A dictionary containing the TODO items for all users.
-  """
+    """
 
-  # Get the TODO items for all users.
-  response = requests.get('https://jsonplaceholder.typicode.com/todos/')
-  todos = response.json()
+    # Get the TODO items for all users.
+    response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todos = response.json()
 
-  # Get the users.
-  response = requests.get('https://jsonplaceholder.typicode.com/users/')
-  users = response.json()
+    # Get the users.
+    response = requests.get('https://jsonplaceholder.typicode.com/users/')
+    users = response.json()
 
-  # Create a dictionary to store the TODO items for all users.
-  user_todos = {}
+    # Create a dictionary to store the TODO items for all users.
+    user_todos = {}
   for user in users:
     user_todos[user['id']] = []
 
@@ -34,12 +34,12 @@ def export_json():
       'completed': todo['completed']
     })
 
-  return user_todos
+    return user_todos
 
 if __name__ == '__main__':
   # Export the data in JSON format.
-  user_todos = export_json()
+    user_todos = export_json()
 
   # Write the JSON data to a file
   with open('todo_all_employees.json', 'w') as f:
-                        json.dump(user_todos, f)
+      json.dump(user_todos, f)
