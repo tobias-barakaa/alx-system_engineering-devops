a5f34d40693974d89254fa63bcc9ae733f687d4c
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 01:08:00 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
+"""
+Script to Fetch Employee TODO List Progress
+
+This script accepts an employee ID as a parameter and retrieves information
+about the employee's TODO list progress from a REST API. It fetches data from
+the 'jsonplaceholder.typicode.com' API and displays
+the progress in a specific format.
+
+Usage:
+    python3 gather_data_from_an_API.py <employee_id>
+
+Parameters:
+    - <employee_id>: An integer representing the employee's ID.
+
+Example:
+    To check the TODO list progress for an employee with ID 2:
+    python3 gather_data_from_an_API.py 2
+
+The script uses the 'requests' library to make HTTP GET requests to the API
+and calculates the number of completed and total tasks. It then displays the
+employee's progress in the following format:
+
+Employee EMPLOYEE_NAME is done with tasks
+(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+    TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+
+Author: [Your Name]
+"""
 
 import requests
 import csv
