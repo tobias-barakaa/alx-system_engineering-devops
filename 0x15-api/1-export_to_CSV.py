11a63cee413fac178044195885a8ca19eb4e210d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 01:12:01 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,64 +1,35 @@
 #!/usr/bin/python3
+
 """
-Script to Fetch Employee TODO List Progress
-
-This script accepts an employee ID as a parameter and retrieves information
-about the employee's TODO list progress from a REST API. It fetches data from
-the 'jsonplaceholder.typicode.com' API and displays
-the progress in a specific format.
-
-Usage:
-    python3 gather_data_from_an_API.py <employee_id>
-
-Parameters:
-    - <employee_id>: An integer representing the employee's ID.
-
-Example:
-    To check the TODO list progress for an employee with ID 2:
-    python3 gather_data_from_an_API.py 2
-
-The script uses the 'requests' library to make HTTP GET requests to the API
-and calculates the number of completed and total tasks. It then displays the
-employee's progress in the following format:
-
-Employee EMPLOYEE_NAME is done with tasks
-(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-    TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-
-Author: [Your Name]
+Python script that exports data in the CSV format
 """
 
-import requests
-import csv
+from requests import get
 from sys import argv
+import csv
 
 if __name__ == "__main__":
-    employee_id = int(argv[1])
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    # Fetch employee data from the REST API
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data.get('username')
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    # Fetch TODO list data from the REST API
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            employee = i['username']
 
-    # Create a CSV file for the employee
-    csv_filename = f'{employee_id}.csv'
-    with open(csv_filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        
-        # Write the CSV header
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        
-        # Write the tasks owned by the employee to the CSV file
-        for task in todos_data:
-            if task['userId'] == employee_id:
-                task_completed = str(task['completed'])
-                task_title = task['title']
-                csv_writer.writerow([employee_id, employee_name, task_completed, task_title])
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    print(f"Data has been exported to {csv_filename}.")
+        for i in data:
+
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(employee)
+                row.append(i['completed'])
+                row.append(i['title'])
+
+                writ.writerow(row)
