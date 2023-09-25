d7a258bdbd7e391bbd08f7ccf04d1975db488e4b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-25 22:22:16 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,22 +8,25 @@ returns information about his/her TODO list progress.
 from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
-    all = 0
-    res = get('https://jsonplaceholder.typicode.com/todos/').json()
-    result = []
-    res1 = get('https://jsonplaceholder.typicode.com/users/').json()
+     all = 0
+     res = get('https://jsonplaceholder.typicode.com/todos/').json()
+     result = []
+     res1 = get('https://jsonplaceholder.typicode.com/users/')
 
-    for i in res1:
-        if i.get('id') == int(argv[1]):
-            users = i.get('name')
-        elif i.get('userId') == int(argv[1]):
-            all += 1
-        elif i.get('completed') is not False:
-            all += 1
-            result.append(i.get('title'))
+     for i in res1:
+         if i.get('id') == int(argv[1]):
+         users = i.get('name')
+      for i in res:
+         if (i.get('userId') == int(argv[1])):
+         all += 1
+         elif (i.get('completed') is not false):
+         all + 1
+         result.append(i.get('title'))
 
-    print("Employee {} is done with tasks({}/{}):".format(users, all, len(res)))
+     print("Employee {} is done with tasks({}/{}):".format(users, completed,
+                                                          result))
 
     for i in result:
-        print("\t{}".format(i))
+        print("\t {}".format(i))
