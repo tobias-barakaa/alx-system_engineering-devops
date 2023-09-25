dfa6927b65268ffdb53cb162a8bfbf3bf31eb7ec
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-26 02:06:45 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,53 +4,55 @@
 Python script that exports data in the JSON format.
 """
 
-import requests
-import sys
+# Documentation:
+# This Python script is designed to fetch data from a public REST API and export it in JSON format.
+# It retrieves a list of tasks associated with a specific employee ID from the JSONPlaceholder API
+# and organizes the data into a JSON object. The JSON object is then written to a JSON file with
+# the filename being the employee's ID.
+
+# Usage:
+# To use this script, run it from the command line with an employee ID as an argument. For example:
+#   python3 script.py 2
+# This will fetch the tasks associated with the employee ID 2 and export the data to a JSON file
+# named "2.json" in the current directory.
+
+# Requirements:
+# - Python 3
+# - The `requests` module for making HTTP requests. You can install it using pip:
+#   pip install requests
+
 import json
-
-def export_todo_list_to_json(user_id):
-    # API endpoints
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-
-    try:
-        # Fetch user data
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-
-        if 'username' not in user_data:
-            print(f"Employee with ID {user_id} not found.")
-            return
-
-        username = user_data['username']
-
-        # Fetch TODO list data for the user
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        user_dict = {}
-        user_dict[username] = []
-
-        for task in todos_data:
-            if 'userId' in task:
-                user_dict[username].append({
-                    'task': task['title'],
-                    'completed': task['completed']
-                })
-
-        with open(f'{user_id}.json', 'w') as json_file:
-            json.dump(user_dict, json_file, indent=4)
-
-        print(f"Data exported to {user_id}.json")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return
+from requests import get
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    user_id = int(sys.argv[1])
-    export_todo_list_to_json(user_id)
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            u_name = i['username']
+            id_no = i['id']
+
+    row = []
+
+    for i in data:
+
+        new_dict = {}
+
+        if i['userId'] == int(argv[1]):
+            new_dict['username'] = u_name
+            new_dict['task'] = i['title']
+            new_dict['completed'] = i['completed']
+            row.append(new_dict)
+
+    final_dict = {}
+    final_dict[id_no] = row
+    json_obj = json.dumps(final_dict)
+
+    with open(argv[1] + ".json",  "w") as f:
+        f.write(json_obj)
