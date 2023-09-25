a1be6dad397ca146180931e99a871439e7926043
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-25 22:19:43 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,38 +8,22 @@ returns information about his/her TODO list progress.
 from requests import get
 from sys import argv
 
-def get_employee_todo_progress(employee_id):
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-
-    completed = 0
-    total = 0
-    tasks = []
-
-    for i in data:
-        if i.get('userId') == int(employee_id):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    return {
-        "employee_name": employee_id,
-        "number_of_done_tasks": completed,
-        "total_number_of_tasks": total,
-        "task_titles": tasks
-    }
-
 if __name__ == "__main__":
-    employee_id = int(argv[1])
-    employee_todo_progress = get_employee_todo_progress(employee_id)
+    all = 0
+    res = get('https://jsonplaceholder.typicode.com/todos/').json()
+    result = []
+    res1 = get('https://jsonplaceholder.typicode.com/users/').json()
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_todo_progress["employee_name"],
-        employee_todo_progress["number_of_done_tasks"],
-        employee_todo_progress["total_number_of_tasks"]
-    ))
+    for i in res1:
+        if i.get('id') == int(argv[1]):
+            users = i.get('name')
+        elif i.get('userId') == int(argv[1]):
+            all += 1
+        elif i.get('completed') is not False:
+            all += 1
+            result.append(i.get('title'))
 
-    for task_title in employee_todo_progress["task_titles"]:
-        print("\t {}".format(task_title))
+    print("Employee {} is done with tasks({}/{}):".format(users, all, len(res)))
+
+    for i in result:
+        print("\t{}".format(i))
