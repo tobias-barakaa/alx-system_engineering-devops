d545f928b9e82478c0a6ba02e1a612439cd21f0e
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-10-11 01:13:25 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
+# import
+from requests import get
+from sys import argv
 
-"""
-import
-"""
+
+def recurse(subreddit, hotlist=[], after=None):
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    try:
+        if after:
+            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head).json().get('data')
+        else:
+            count = get('https://www.reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head).json().get('data')
+        hotlist += [dic.get('data').get('title')
+                    for dic in count.get('children')]
+        if count.get('after'):
+            return recurse(subreddit, hotlist, after=count.get('after'))
+        return hotlist
+    except:
+        return None
 
 
-import requests
-import sys
-
-def get_hot_posts(subreddit):
-  """Recursively gets the hot posts for a given subreddit.
-
-  Args:
-    subreddit: The name of the subreddit to get the hot posts for.
-
-  Returns:
-    A list of the titles of the hot posts.
-  """
-
-  after = None
-  hotlist = []
-  while True:
-    headers = {'User-Agent': 'Dan Kazam'}
-    if after:
-      url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
-          subreddit, after)
-    else:
-      url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    data = response.json().get('data')
-    if not data:
-      break
-    hotlist += [dic.get('data').get('title') for dic in data.get('children')]
-    after = data.get('after')
-  return hotlist
-
-if __name__ == '__main__':
-  subreddit = sys.argv[1]
-  hot_posts = get_hot_posts(subreddit)
-  for post in hot_posts:
-    print(post)
+if __name__ == "__main__":
+    recurse(argv[1])
