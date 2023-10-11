47e9789d630883bdb19f55d42af15cec105f2901
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-10-11 23:06:55 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,7 @@
 # fixes`
 
-file { '/etc/apache2/sites-available/your_site.conf':
-  ensure => present,
-  source => 'puppet:///modules/your_module/your_site.conf',
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/apache2/sites-available/your_site.conf'],
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  replace => 'true',
+  content => file('path/to/your/wp-settings.php'), # Replace with the path to your original wp-settings.php
 }
